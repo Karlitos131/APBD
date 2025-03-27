456018e6020c01c4ef58dd3dcb777ae689f073ef
Karlitos131 <s31148@pjwstk.edu.pl>
2025-03-27 18:36:36 +0100

    a

--- obj/Debug/net9.0/APBD.AssemblyInfo.cs
+++ obj/Debug/net9.0/APBD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b933142420590194de01ed80c86429d141bf5ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e83f0f96d31600c0e63fd4d5c349154de0605b")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
